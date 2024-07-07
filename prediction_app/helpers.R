7f57ec1fff3017a8c5bcb87c77867d5464141b7c
davidfg83 <davidfgonzalez@gmail.com>
2024-07-07 17:33:55 +0200

    Function now offers unique prediction with backstop model Lemmatized instead of stemming the corpus Defined pre-processing function of user text outside prediction function

--- prediction_app/helpers.R
+++ prediction_app/helpers.R
@@ -2,73 +2,72 @@
 
 library(dplyr)
 library(stringr)
-library(tm)
+library(textstem)
 
-# Define a function using a backoff model to offer 6 suggestions: 2 from bigrams without stop words, 2 from the bigrams with stop words, 2 from the co-occurrence matrix. 
-predict_next_word <- function(sentence, unigrams_tbl, bigrams_tbl, bigrams_no_stop_tbl, co_occurrence_df) {
+# Preprocess text inputed by user
+preprocess_text <- function(text) {
+        text <- tolower(text)
+        text <- gsub("[[:digit:]]", "", text)
+        text <- gsub("[[:punct:]]", "", text)
+        text <- gsub("\\s+", " ", text)
+        text <- trimws(text)
+        text <- lemmatize_words(text)
+        return(text)
+}
+
+# Define a function using a backoff model
+predict_next_word <- function(sentence, unigrams_tbl, bigrams_tbl, trigrams_tbl, co_occurrence_df) {
         # Preprocess the sentence
-        sentence <- tolower(sentence)
-        sentence <- gsub("[[:digit:]]", "", sentence)
-        sentence <- gsub("[[:punct:]]", "", sentence)
-        sentence <- gsub("\\s+", " ", sentence)
-        sentence <- trimws(sentence)
-        sentence_corpus <- Corpus(VectorSource(sentence)) #convert to corpus for stemming
-        sentence_corpus <- suppressWarnings(tm_map(sentence_corpus,stemDocument)) #stemming
-        sentence <- sapply(sentence_corpus, as.character)
+        sentence <- preprocess_text(sentence)
         
         # Split the sentence into words
         words <- unlist(strsplit(sentence, " "))
         num_words <- length(words)
         
-        suggestions <- character(0)
+        if (num_words >= 2) {
+                cont <- paste(words[(num_words-1):num_words], collapse = " ")
+                trigram_suggestion <- trigrams_tbl %>%
+                        filter(context == cont) %>%
+                        arrange(desc(prob)) %>%
+                        head(1) %>%
+                        pull(ngram) %>%
+                        word(3)
+                
+                if (length(trigram_suggestion) > 0) {
+                        return(trigram_suggestion)
+                }
+        }
         
         if (num_words >= 1) {
                 last_word <- words[num_words]
                 
-                # 1. Try to find suggestions from bigrams_no_stop_tbl
-                bigrams_no_stop_suggestions <- bigrams_no_stop_tbl %>%
+                bigrams_suggestion <- bigrams_tbl %>%
                         filter(context == last_word) %>%
                         arrange(desc(prob)) %>%
-                        head(2) %>%
+                        head(1) %>%
                         pull(ngram) %>%
                         word(2)
                 
-                suggestions <- unique(c(suggestions, bigrams_no_stop_suggestions))
-                
-                # 2. Try to find suggestions from bigrams_tbl
-                bigrams_suggestions <- bigrams_tbl %>%
-                        filter(context == last_word) %>%
-                        arrange(desc(prob)) %>%
-                        head(2) %>%
-                        pull(ngram) %>%
-                        word(2)
-                
-                suggestions <- unique(c(suggestions, bigrams_suggestions))
+                if (length(bigrams_suggestion) > 0) {
+                        return(bigrams_suggestion)
+                }
         }
         
-        # 3. Try to find suggestions from co_occurrence_df
-        co_occurrence_suggestions <- co_occurrence_df %>%
+        co_occurrence_suggestion <- co_occurrence_df %>%
                 filter(term1 %in% words | term2 %in% words) %>%
                 arrange(desc(Freq)) %>%
-                head(2) %>%
+                head(1) %>%
                 mutate(word = if_else(term1 %in% words, term2, term1)) %>%
                 pull(word)
         
-        suggestions <- unique(c(suggestions, co_occurrence_suggestions))
-        
-        # Remove invalid suggestions (e.g., digits, NA, empty strings)
-        suggestions <- suggestions[!is.na(suggestions) & suggestions != "" & !grepl("[[:digit:]]", suggestions)]
-        
-        # 4. If fewer than 6 suggestions, fill with unigrams_tbl
-        if (length(suggestions) < 6) {
-                unigram_suggestions <- unigrams_tbl %>%
-                        arrange(desc(prob)) %>%
-                        head(6 - length(suggestions)) %>%
-                        pull(ngram)
-                
-                suggestions <- unique(c(suggestions, unigram_suggestions))
+        if (length(co_occurrence_suggestion) > 0) {
+                return(co_occurrence_suggestion)
         }
         
-        # Return the top 6 unique suggestions
-        return(suggestions[1:6])
+        unigram_suggestion <- unigrams_tbl %>%
+                arrange(desc(prob)) %>%
+                head(1) %>%
+                pull(ngram)
+        
+        return(unigram_suggestion)
 }
